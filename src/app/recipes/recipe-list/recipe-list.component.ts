01aa10483f5cc13c0aa4870f8b991acb8da1e045
Poorvanshi <poorvanshi2829@gmail.com>
2023-08-18 16:27:07 -0400

    changes in reciepe components and shopping list components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply test', 'https://assets.epicurious.com/photos/5df7c9efa9f50600094dffed/1:1/w_2240,c_limit/COOK90_ButterPaneer_RECIPE_V2_121119_288.jpg'),
+    new Recipe('Test Recipe', 'This is simply test', 'https://assets.epicurious.com/photos/5df7c9efa9f50600094dffed/1:1/w_2240,c_limit/COOK90_ButterPaneer_RECIPE_V2_121119_288.jpg')
+  ];
 
   ngOnInit() {
     
