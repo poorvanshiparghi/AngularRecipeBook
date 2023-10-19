94bbbf7ac95fd7fea1b2b2ad644ef6603065534b
Poorvanshi <poorvanshi2829@gmail.com>
2023-10-19 10:58:41 -0400

    clean up

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[];
+  private igChangedSub: Subscription;
 
   constructor(private slservice: ShoppingListService){}
 
   ngOnInit(){
     this.ingredients = this.slservice.getIngredients();
-    this.slservice.ingredientsChanged
+    this.igChangedSub = this.slservice.ingredientsChanged
     .subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
+  }
+
+  ngOnDestroy() {
+    this.igChangedSub.unsubscribe();
   }
 
 }
